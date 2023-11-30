bd6931a791a9e2cbc672e60e61027a00e0d1bad0
DarrenKwonDev <darrenkwondev46@gmail.com>
2023-12-01 02:17:02 +0900

    chore: comment

--- src/ECS/ECS.h
+++ src/ECS/ECS.h
@@ -11,6 +11,7 @@ typedef std::bitset<MAX_COMPONENTS> Signature;
 
 ////////////////////////////////////////////////////////
 // component. pure data
+// template class이므로 컴파일 시점에 여러 개의 클래스로 분화된다는 것을 의미.
 ////////////////////////////////////////////////////////
 
 struct IComponent {
@@ -65,6 +66,7 @@ public:
 };
 
 ////////////////////////////////////////////////////////
+// system
 // mutate state in component that entities has
 ////////////////////////////////////////////////////////
 class System {
@@ -85,16 +87,6 @@ public:
     void RequireComponent();
 };
 
-////////////////////////////////////////////////////////
-// mutate state in component that entities has
-////////////////////////////////////////////////////////
-class Registry {
-private:
-public:
-    Registry();
-    virtual ~Registry();
-};
-
 // 템플릿은 제네릭과 다르게 컴파일 시간에 인스턴스화 됨.
 // 따라서 템플릿의 구현을 별도의 .cpp 파일에 넣으면,
 // 컴파일러가 다른 소스 파일에서 해당 템플릿을 인스턴스화할 때 필요한 정보를 찾을 수 없게 됨.
@@ -103,4 +95,24 @@ template <typename TComponent>
 inline void System::RequireComponent() {
     const auto componentId = Component<TComponent>::GetId();
     mComponentSignature.set(componentId);
-}
+};
+
+////////////////////////////////////////////////////////
+// registry
+// 흔히 singleton으로 구현되는 game manager
+////////////////////////////////////////////////////////
+class Registry {
+private:
+    unsigned int mNumEntities = 0;
+
+public:
+    Registry();
+    virtual ~Registry();
+
+    Entity CreateEntity();
+    void KillEntity();
+    void AddComponent();
+    void RemoveComponent();
+    void AddSystem();
+    void RemoveSystem();
+};
